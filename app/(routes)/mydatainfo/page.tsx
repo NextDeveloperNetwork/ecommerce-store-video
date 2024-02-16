c5580adbe4c98186036e578db3b370a1661a03d1
BlackKnightNET <g3albania.elvis@gmail.com>
2024-02-16 13:14:40 +0100

    final_B4_S_1.0.5.1

---
+++ app/(routes)/mydatainfo/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const EdiStoreTerms = async () => {
+  return (
+    <div className="max-w-3xl mx-auto p-8 bg-white rounded-md shadow-lg">
+      <h1 className="text-4xl font-bold mb-8">My info</h1>
+
+      <div className="mb-8">
+        <h2 className="text-2xl font-bold mb-4">email accounts</h2>
+        <p className="mb-4">
+          elvis.simaku@yahoo.com
+          store.edi.al@gmail.com
+          g3albania.elvis@gmail.com
+        </p>
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default EdiStoreTerms;
