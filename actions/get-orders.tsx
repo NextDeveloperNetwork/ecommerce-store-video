4f750b9eb8c5fd77dc36753d53f9fa2ea5e3fbb7
BlackKnightNET <g3albania.elvis@gmail.com>
2024-06-22 14:20:12 +0200

    final_B4_S_1.0.8.9_OrderPage_Chashing_Problem

--- actions/get-orders.tsx
+++ actions/get-orders.tsx
@@ -4,7 +4,7 @@ import { Order } from "@/types";
 const URL=`${process.env.NEXT_PUBLIC_API_URL}/order`;
 
 const getOrder = async (id: string): Promise<Order[]> => {
-  const res = await fetch(URL);
+  const res = await fetch(`${URL}/${id}`);
 
   return res.json();
 };
