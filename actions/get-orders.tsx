e8b44b15efa8ef352c19daf2a56db7e39b4c2114
BlackKnightNET <g3albania.elvis@gmail.com>
2024-12-02 10:05:26 +0100

    reloads

--- actions/get-orders.tsx
+++ actions/get-orders.tsx
@@ -3,6 +3,7 @@ import { Order } from "@/types";
 
 const URL=`${process.env.NEXT_PUBLIC_API_URL}/order`;
 
+
 const getOrder = async (id: string): Promise<Order[]> => {
   const res = await fetch(`${URL}/${id}`);
 
